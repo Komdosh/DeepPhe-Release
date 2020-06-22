44bf963c8d8e4b8f8c5631e1485a0672a15e1a7a
Andrey Tabakov <komdosh@yandex.ru>
2020-06-22 09:50:57 +0300

    neo4j output graph recreate externally add simple segment for sectionizer

--- deepphe-ctakes-cancer/src/main/java/org/apache/ctakes/neo4j/Neo4jConnectionFactory.java
+++ deepphe-ctakes-cancer/src/main/java/org/apache/ctakes/neo4j/Neo4jConnectionFactory.java
@@ -19,49 +19,60 @@ import java.io.IOException;
 public enum Neo4jConnectionFactory {
    INSTANCE;
 
+   static private final Logger LOGGER = Logger.getLogger("Neo4jConnectionFactory");
+   static private final String STATIC_GRAPH_DB = "resources/org/healthnlp/deepphe/graph/neo4j/test.db";
+   static private final String OUTPUT_GRAPH_DB = "output_graph/deepphe.db";
+   private GraphDatabaseService _graphDb;
+
    public static Neo4jConnectionFactory getInstance() {
       return INSTANCE;
    }
 
-   static private final Logger LOGGER = Logger.getLogger( "Neo4jConnectionFactory" );
-
-   static private final String STATIC_GRAPH_DB = "resources/org/healthnlp/deepphe/graph/neo4j/test.db";
-   static private final String OUTPUT_GRAPH_DB = "output_graph/deepphe.db";
-
-   private GraphDatabaseService _graphDb;
-
-   private void createOutputGraph( final String outputDbPath ) {
-      final File outputGraph = new File( outputDbPath );
-      final String path = outputGraph.getAbsolutePath();
-      if ( !outputGraph.exists() ) {
-         LOGGER.info( "Creating output Graph " + path );
-         outputGraph.mkdirs();
-      } else {
-         LOGGER.info( "Replacing existing output Graph " + path );
-         deleteDirectory( outputGraph );
-      }
-      final File defaultGraph = new File( getStaticGraphDb() );
-      try {
-         FileUtils.copyDirectory( defaultGraph, outputGraph, false );
-      } catch ( IOException ioE ) {
-         LOGGER.error( ioE.getMessage(), ioE );
-         System.exit( 1 );
-      }
-   }
-
-   static private boolean deleteDirectory( final File directory ) {
+   static private boolean deleteDirectory(final File directory) {
       final File[] files = directory.listFiles();
-      if ( files == null ) {
+      if (files == null) {
          return true;
       }
       boolean ok = true;
-      for ( File file : files ) {
-         if ( file.isDirectory() ) {
-            ok &= deleteDirectory( file );
+      for (File file : files) {
+         if (file.isDirectory()) {
+            ok &= deleteDirectory(file);
          }
          ok &= file.delete();
       }
       return ok;
+   }
+
+   static private void registerShutdownHook(final GraphDatabaseService graphDb) {
+      // Registers a shutdown hook for the Neo4j instance so that it
+      // shuts down nicely when the VM exits (even if you "Ctrl-C" the
+      // running application).
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+         try {
+            graphDb.shutdown();
+         } catch (LifecycleException | RotationTimeoutException multE) {
+            // ignore
+         }
+      }));
+   }
+
+   private void createOutputGraph(final String outputDbPath) {
+      final File outputGraph = new File(outputDbPath);
+      final String path = outputGraph.getAbsolutePath();
+      if (!outputGraph.exists()) {
+         LOGGER.info("Creating output Graph " + path);
+         outputGraph.mkdirs();
+      } else {
+         LOGGER.info("Replacing existing output Graph " + path);
+         deleteDirectory(outputGraph);
+      }
+      final File defaultGraph = new File(getStaticGraphDb());
+      try {
+         FileUtils.copyDirectory(defaultGraph, outputGraph, false);
+      } catch (IOException ioE) {
+         LOGGER.error(ioE.getMessage(), ioE);
+         System.exit(1);
+      }
    }
 
    private String getStaticGraphDb() {
@@ -73,47 +84,35 @@ public enum Neo4jConnectionFactory {
    }
 
    public GraphDatabaseService connectToGraph() {
-      return connectToGraph( getOutputGraphDb() );
+      return connectToGraph(getOutputGraphDb());
    }
 
-   synchronized public GraphDatabaseService connectToGraph( final String graphDbPath ) {
-      if ( _graphDb != null ) {
+   synchronized public GraphDatabaseService connectToGraph(final String graphDbPath) {
+      if (_graphDb != null) {
          return _graphDb;
       }
-      createOutputGraph( graphDbPath );
-      final File graphDbFile = new File( graphDbPath );
-      if ( !graphDbFile.isDirectory() ) {
-         LOGGER.error( "No Database exists at: " + graphDbPath );
-         System.exit( -1 );
+//      createOutputGraph( graphDbPath );        //do it on your app
+
+      final File graphDbFile = new File(graphDbPath);
+      if (!graphDbFile.isDirectory()) {
+         LOGGER.error("No Database exists at: " + graphDbPath);
+         System.exit(-1);
       }
       _graphDb = new GraphDatabaseFactory()
-      .newEmbeddedDatabase( graphDbFile );
-      if ( !_graphDb.isAvailable( 500 ) ) {
-         LOGGER.error( "Could not initialize neo4j connection for: " + graphDbPath );
-         System.exit( -1 );
+          .newEmbeddedDatabase(graphDbFile);
+      if (!_graphDb.isAvailable(500)) {
+         LOGGER.error("Could not initialize neo4j connection for: " + graphDbPath);
+         System.exit(-1);
       }
-      registerShutdownHook( _graphDb );
+      registerShutdownHook(_graphDb);
       return _graphDb;
    }
 
    public GraphDatabaseService getGraph() {
-      if ( _graphDb == null ) {
+      if (_graphDb == null) {
          return connectToGraph();
       }
       return _graphDb;
-   }
-
-   static private void registerShutdownHook( final GraphDatabaseService graphDb ) {
-      // Registers a shutdown hook for the Neo4j instance so that it
-      // shuts down nicely when the VM exits (even if you "Ctrl-C" the
-      // running application).
-      Runtime.getRuntime().addShutdownHook( new Thread( () -> {
-         try {
-            graphDb.shutdown();
-         } catch ( LifecycleException | RotationTimeoutException multE ) {
-            // ignore
-         }
-      } ) );
    }
 
 
